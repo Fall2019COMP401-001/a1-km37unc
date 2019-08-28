72b6f6ee506016770c44076d487d9cdafff6d11b
Kristina <km37@live.unc.edu>
2019-08-27 22:23:35 -0400

    initial commit

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -1,5 +1,6 @@
 package a1;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class A1Jedi {
@@ -8,7 +9,137 @@ public class A1Jedi {
 		
 		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
+		int numberOfItems = scan.nextInt();
+		
+		String[] itemNames = new String[numberOfItems];
+		double[] itemPrices = new double[numberOfItems];
+		
+		for (int i = 0; i < numberOfItems; i++) {
+			
+			itemNames[i] = scan.next();
+			itemPrices[i] = scan.nextDouble();
+			
+		}
+		
+		int numberOfCustomers = scan.nextInt();
+		
+		String[] firstNames = new String[numberOfCustomers];
+		String[] lastNames = new String[numberOfCustomers];
+		int[] boughtItems = new int[numberOfCustomers];
+		
+		int[][] numberOfItemInstances = new int[numberOfCustomers][numberOfItems];
+		
+		for (int i = 0; i < numberOfCustomers; i++) {
+			
+			firstNames[i] = scan.next();
+			lastNames[i] = scan.next();
+			boughtItems[i] = scan.nextInt();
+			
+			int[] boughtItemQuantities = new int[boughtItems[i]];
+			String[] boughtItemNames = new String[boughtItems[i]];
+			
+			for (int j = 0; j < boughtItems[i]; j++) {
+				
+				boughtItemQuantities[j] = scan.nextInt();
+				boughtItemNames[j] = scan.next();
+				
+				numberOfItemInstances[i][getStringIndex(itemNames, boughtItemNames[j])] = boughtItemQuantities[j];
+				
+			}
+						
+		}
+		
+		for (int i = 0; i < numberOfItems; i++) {
+			
+			if (countItems(numberOfItemInstances, i) == 0) {
+				System.out.println("No customers bought " + itemNames[i]);
+			} else {
+				
+				int itemOccurrenceCount = countItems(numberOfItemInstances, i);
+				int itemTotalCount = sumIntArrayColumn(numberOfItemInstances, i);
+				
+				System.out.println(itemOccurrenceCount + " customers bought " + itemTotalCount + " " + itemNames[i]);
+			}
+			
+		}
+		
+		scan.close();
+	}
+	
+	public static int getStringIndex(String[] array, String findString) {
+		
+		int j = 0;
+		
+		for (int i = 0; i < array.length; i++) {
+			if (findString.equals(array[i])) {
+				j = i;
+			}
+		}
+		
+		return j;
+	}
+	
+	public static int getSmallestIndex(double[] array) {
+		
+		int j = 0;
+		
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] < array[j]) {
+				j = i;
+			}
+		}
+		
+		return j;
+	}
+	
+	public static int getLargestIndex(double[] array) {
+		
+		int j = 0;
+		
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] > array[j]) {
+				j = i;
+			}
+		}
+		
+		return j;
+	}
+	
+	public static int sumIntArray(int[] array) {
+		
+		int sum = 0;
+		
+		for (int i = 0; i < array.length; i++) {
+			sum += array[i];
+		}
+		
+		return sum;
+	}
+	
+	public static int sumIntArrayColumn(int[][] array, int column) {
+		
+		int sum = 0;
+		
+		for (int i = 0; i < array.length; i++) {
+			sum += array[i][column];
+		}
+		
+		return sum;
+	}
+
+
+	public static int countItems(int[][] array, int column) {
+		
+		int count = 0;
+		
+		for (int i = 0; i < array[0].length; i++) {
+			if (array[i][column] > 0) {
+				count += 1;
+			}
+		}
+		
+		return count;
 		
 	}
 }
+
